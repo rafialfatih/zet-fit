1bedf00892317214f3462a01833a9d1562f0ed5f
rafi <rafi.alfatih@gmail.com>
2022-01-16 08:52:23 +0700

    fix quotation mark

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -5,14 +5,15 @@ export default function Carousel() {
     <div className="w-full h-full overflow-x-hidden py-10">
       <div className="pb-8 px-12 text-center">
         <h2 className="text-4xl md:text-5xl font-semibold mb-6">
-          It's Time To Get Fit
+          It&lsquo;s Time To Get Fit
         </h2>
         <p className="text-xl md:text-2xl leading-relaxed whitespace-pre-line">
-          People usually confused when it’s time planning a workout program for
-          them. Many people think that building muscle can only be done on a gym
-          and with equipment. It’s time to change that perspective! We provide
-          you a workout plan based on your time, level, muscle group and type.
-          Having a healthy and fit body is everybody rights.
+          People usually confused when it&lsquo;s time planning a workout
+          program for them. Many people think that building muscle can only be
+          done on a gym and with equipment. It&lsquo;s time to change that
+          perspective! We provide you a workout plan based on your time, level,
+          muscle group and type. Having a healthy and fit body is everybody
+          rights.
         </p>
         <p className="my-8 text-3xl font-semibold text-sky-700">
           Get fit with us now!
