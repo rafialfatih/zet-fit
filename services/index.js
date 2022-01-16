cd3a4d3d77020eac2b647fc6e476c2f435fc5d0d
rafi <rafi.alfatih@gmail.com>
2022-01-16 16:09:31 +0700

    add data menu

---
+++ services/index.js
@@ -0,0 +1,18 @@
+export const getMenu = () => {
+  const menu = [
+    {
+      url: "/",
+      title: "Home",
+    },
+    {
+      url: "/workout",
+      title: "Workout Plan",
+    },
+    {
+      url: "/about",
+      title: "About",
+    },
+  ];
+
+  return menu;
+};
